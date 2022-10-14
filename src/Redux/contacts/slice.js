e249ca72dd2449215c9509bf377119a72be95766
Maksym32 <bednogo1@gmail.com>
2022-10-14 18:13:48 +0300

    update

--- src/Redux/contacts/slice.js
+++ src/Redux/contacts/slice.js
@@ -38,7 +38,7 @@ const contactsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const idx = state.items.findIndex(item => item.id === action.payload.id)
+      const idx = state.items.findIndex(item => item.id === action.meta.arg)
         state.items.splice(idx, 1);
     },
     [deleteContact.rejected]: handleRejected,
